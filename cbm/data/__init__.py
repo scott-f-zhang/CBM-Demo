a3c02bb8821000cce933053491bf26b0010b52ba
Scott Zhang <scott.f.zhang@gmail.com>
2025-11-17 22:44:26 -0700

    Prune main: keep only runtime-essential files; move training/data/tests to dev

--- cbm/data/__init__.py
+++
@@ -1,7 +0,0 @@
-from .cebab import CEBaBDataset
-from .imdb import IMDBDataset
-from .essay import EssayDataset
-from .qa import QADataset
-
-__all__ = ["CEBaBDataset", "IMDBDataset", "EssayDataset", "QADataset"]
-
